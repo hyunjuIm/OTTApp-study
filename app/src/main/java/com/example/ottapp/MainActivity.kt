3726c18af195fba9966b602ddf9d681b9fb7c13a
hyunju <dear.jjwim@gmail.com>
2022-05-31 17:20:13 +0900

    AppbarLayout 헤더 애니메이션 구현

--- app/src/main/java/com/example/ottapp/MainActivity.kt
+++ app/src/main/java/com/example/ottapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.ottapp
 
+import android.app.Activity
 import android.content.Context
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.ottapp.databinding.ActivityMainBinding
+import com.google.android.material.appbar.AppBarLayout
+import java.lang.Math.abs
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,6 +25,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        makeStatusBarTransparent()
+
+        initAppBar()
+        initInsetMargin()
 
         binding.scrollView.viewTreeObserver.addOnScrollChangedListener {
             if (binding.scrollView.scrollY > 150f.dpToPx(this).toInt()) {
@@ -53,12 +67,63 @@ class MainActivity : AppCompatActivity() {
                 motionLayout: MotionLayout?, triggerId: Int, positive: Boolean, progress: Float
             ) = Unit
         })
+
     }
 
-    fun Float.dpToPx(context: Context): Float =
-        TypedValue.applyDimension(
-            TypedValue.COMPLEX_UNIT_DIP,
-            this,
-            context.resources.displayMetrics
-        )
+    private fun initInsetMargin() = with(binding) {
+        ViewCompat.setOnApplyWindowInsetsListener(coordinator) { v: View, insets: WindowInsetsCompat ->
+            val params = v.layoutParams as ViewGroup.MarginLayoutParams
+            params.bottomMargin = insets.systemWindowInsetBottom
+            toolbarContainer.layoutParams = (toolbarContainer.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                setMargins(0, insets.systemWindowInsetTop, 0, 0)
+            }
+            collapsingToolbarContainer.layoutParams = (collapsingToolbarContainer.layoutParams as ViewGroup.MarginLayoutParams).apply {
+                setMargins(0, 0, 0, 0)
+            }
+
+            insets.consumeSystemWindowInsets()
+        }
+    }
+
+    private fun initAppBar() {
+        binding.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBarLayout, verticalOffset ->
+            val topPadding = 300f.dpToPx(this)
+            val realAlphaScrollHeight = appBarLayout.measuredHeight - appBarLayout.totalScrollRange
+            val abstractOffset = abs(verticalOffset)
+
+            val realAlphaVerticalOffset = if (abstractOffset - topPadding < 0) 0f else abstractOffset - topPadding
+
+            if (abstractOffset < topPadding) {
+                binding.toolbarBackgroundView.alpha = 0f
+                return@OnOffsetChangedListener
+            }
+            val percentage = realAlphaVerticalOffset / realAlphaScrollHeight
+            binding.toolbarBackgroundView.alpha = 1 - (if (1 - percentage * 2 < 0) 0f else 1 - percentage * 2)
+        })
+        initActionBar()
+    }
+
+    private fun initActionBar() = with(binding) {
+        toolbar.navigationIcon = null
+        toolbar.setContentInsetsAbsolute(0, 0)
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.let {
+            it.setHomeButtonEnabled(false)
+            it.setDisplayHomeAsUpEnabled(false)
+            it.setDisplayShowHomeEnabled(false)
+        }
+    }
+}
+
+fun Float.dpToPx(context: Context): Float =
+    TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
+
+fun Activity.makeStatusBarTransparent() {
+    with(window) {
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        statusBarColor = Color.TRANSPARENT
+    }
 }
