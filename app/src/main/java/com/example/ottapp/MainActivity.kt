b0160ebc2e9261e29c4fed147a0254c6b63408d1
hyunju <dear.jjwim@gmail.com>
2022-05-31 15:42:41 +0900

    버튼 MotionLayout 적용

--- app/src/main/java/com/example/ottapp/MainActivity.kt
+++ app/src/main/java/com/example/ottapp/MainActivity.kt
@@ -21,10 +21,12 @@ class MainActivity : AppCompatActivity() {
             if (binding.scrollView.scrollY > 150f.dpToPx(this).toInt()) {
                 if (isGatheringMotionAnimating.not()) {
                     binding.gatheringDigitalThingsLayout.transitionToEnd()
+                    binding.buttonShownMotionLayout.transitionToEnd()
                 }
             } else {
                 if (isGatheringMotionAnimating.not()) {
                     binding.gatheringDigitalThingsLayout.transitionToStart()
+                    binding.buttonShownMotionLayout.transitionToStart()
                 }
             }
         }
